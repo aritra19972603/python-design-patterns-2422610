2f238d6a07567ff9c866e8baa709ef45bc802cd0
aritra <49690310+aritra19972603@users.noreply.github.com>
2024-05-26 07:06:22 +0000

    updated

--- singleton.py
+++ singleton.py
@@ -1,12 +1,34 @@
 class Borg:
     
     """The Borg design pattern"""
-    
+    _shared_data={}
+
+    def __init__(self):
+        self.__dict__=self._shared_data
 
         
 
 class Singleton(Borg): 
     
     """The singleton class"""
+
+    def __init__(self,**kwargs):
+        Borg.__init__(self)
+        self._shared_data.update(kwargs)
+
+    def __str__(self):
+        return str(self._shared_data)
+    
+
+
+x= Singleton(HTTP = " HYPER TEXT TRANSFER PROTOCAL")
+
+print(x)
+
+y=Singleton(SNPM="SIMPLE NETWORK MANAGEMENT PROTOCOL")
+
+print(y)
+
+
    
 
